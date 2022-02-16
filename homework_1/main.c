e2c9f66c5a59ca06239dee9c2b9565874463e26d
SvetlanaYes <91842237+SvetlanaYes@users.noreply.github.com>
2022-02-16 20:41:55 +0400

    some changes

--- homework_1/main.c
+++ homework_1/main.c
@@ -4,9 +4,11 @@ int main(int argc , char** argv)
 {
     char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
     int value1 = 12;
-    int value2 = 12.6;
+    double value2 = 12.6;
     print_string(greeting,6);
+    printf("\n");
     print_integer(value1);
+    printf("\n");
     print_double(value2);
     return 0;
 }
