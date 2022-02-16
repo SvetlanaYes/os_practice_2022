d2af1c654aaf42a7dcab922d470287856614aaf7
SvetlanaYes <yesayan.svetlana@student.rau.am>
2022-02-16 17:58:03 +0400

    added my first homework!

---
+++ homework_1/hello.c
@@ -0,0 +1,9 @@
+#include <stddef.h>
+#include <stdio.h>
+
+#include "hello.h"
+
+void say_hello()
+{
+    printf("%s","hello!\n");
+}
---
+++ homework_1/hello.h
@@ -0,0 +1,6 @@
+#ifndef _HELLO_H__
+#define __Hello_H__
+
+void say_hello();
+
+#endif
