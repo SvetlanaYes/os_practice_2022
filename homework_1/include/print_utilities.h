0213fc1d944f8f25865fd2d38a7edaa7cce23be0
SvetlanaYes <91842237+SvetlanaYes@users.noreply.github.com>
2022-02-16 21:14:48 +0400

    mv .h file to include

---
+++ homework_1/include/print_utilities.h
@@ -0,0 +1,11 @@
+#ifndef _PRINT_H__
+#define __Print_H__
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_string(const char* str, const size_t size);
+void print_integer(const int int_value);
+void print_double(const double double_value);
+
+#endif
